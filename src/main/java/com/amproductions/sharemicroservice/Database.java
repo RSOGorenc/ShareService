ee6a24d619902b5e3927108baff3be68c8cfcf0f
Aljaz <ar1961@student.uni-lj.si>
2020-01-16 08:42:40 +0100

    change mongo URL. health and database

--- src/main/java/com/amproductions/sharemicroservice/Database.java
+++ src/main/java/com/amproductions/sharemicroservice/Database.java
@@ -19,7 +19,7 @@ import static com.mongodb.client.model.Filters.eq;
 
 
 class Database {
-    private static MongoClient mongoClient = new MongoClient(new MongoClientURI("mongodb://35.240.42.72:27017"));
+    private static MongoClient mongoClient = new MongoClient(new MongoClientURI("mongodb://rso-mongo-service:27017"));
     private static MongoDatabase database = mongoClient.getDatabase("imagePlatform");
     private static MongoCollection collection = database.getCollection("images");
 
